f1745b325006b21d913b78c667dbc6b9c526e229
awb99 <awb99@gmail.com>
2023-08-15 11:58:19 -0500

    notebook fas added

--- ui-fonts/resources/demo/notebook/fonts.cljs
+++ ui-fonts/resources/demo/notebook/fonts.cljs
@@ -1,12 +1,21 @@
 (ns demo.notebook.fonts)
 
 ^:R
-[:div {:style {:font-family "Roboto"
+[:div
+
+ [:h1 "Fontawesome example"]
+ [:i.fas.fa-check]
+ 
+ [:hr]
+ [:h1 "Google Font Roboto Example:"]
+ 
+ [:div {:style {:font-family "Roboto"
                :font-size "22px"}}
- [:h1 "Roboto"]
- [:p "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."]
- [:p "123456790"]
- [:p "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
- [:p "abcdefghijklmnopqrstuvwxyz"]
+    [:h1 "Roboto"]
+    [:p "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."]
+    [:p "123456790"]
+    [:p "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+   [:p "abcdefghijklmnopqrstuvwxyz"]]
+
  ]
 
